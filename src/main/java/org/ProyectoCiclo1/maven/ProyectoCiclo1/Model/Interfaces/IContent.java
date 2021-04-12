10224b9954cf2a567acfbcb554408a2a507f7f1a
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-12 14:22:27 +0200

    Commented IContent.java

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IContent.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IContent.java
@@ -4,7 +4,7 @@ public interface IContent{
 	
 	public Integer getID();
 	public Entries getEntry();
-	public Integer getType();
-	public String getValue();
+	public Integer getType();	//Returns 0 (Image) or 1 (Link)
+	public String getValue();	//Returns an image stored on the database as Base64 or the URL
 	
 }
