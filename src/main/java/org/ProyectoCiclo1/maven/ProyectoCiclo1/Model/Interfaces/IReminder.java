e209871277e668473876733fa297ba079faba338
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-12 14:19:03 +0200

    Created IReminder.java

---
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IReminder.java
@@ -0,0 +1,10 @@
+package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces;
+
+import java.time.LocalDateTime;
+
+public interface IReminder {
+	
+	public LocalDateTime getDate();
+	public Integer getStatus();		//Returns 0 (Pending) or 1 (Done)
+	
+}
