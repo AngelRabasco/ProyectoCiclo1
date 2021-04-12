2a786666c62aef145a0b27d1d379a5e3c50d602c
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-12 14:13:21 +0200

    Created IContent.java

---
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IContent.java
@@ -0,0 +1,10 @@
+package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces;
+
+public interface IContent{
+	
+	public Integer getID();
+	public Entries getEntry();
+	public Integer getType();
+	public String getValue();
+	
+}
