30500beea185d4623976d12c8a7c729a1065a4d2
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-13 13:23:06 +0200

    Added constructors to User.java

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Users/User.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Users/User.java
@@ -2,9 +2,17 @@ package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Users;
 
 import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces.IUser;
 
-public class User implements IUser{
+public class User implements IUser {
 	private Integer ID;
 	private String name;
+	
+	public User() {
+		
+	}
+	public User(Integer ID, String name) {
+		this.ID=ID;
+		this.name=name;
+	}
 	
 	public Integer getID() {
 		return this.ID;
