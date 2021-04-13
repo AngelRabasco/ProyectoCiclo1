fb6a5b7c913f1a6e1103c7f78671e20bfc244a75
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-13 14:42:54 +0200

    Removed getEntries() from ISubject.java

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/ISubject.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/ISubject.java
@@ -1,12 +1,9 @@
 package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces;
 
-import java.util.List;
-
 public interface ISubject {
 	
 	public Integer getID();
 	public String getName();
-	public List<IEntry> getEntries();
 	public void setName(String name);
 	
 }
