a458cc846467b72f3a96ccc7ec298e1e6fa00f3a
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-20 11:26:09 +0200

    Added java.sql requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.ProyectoCiclo1.maven.ProyectoCiclo1 {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.sql;
 
     opens org.ProyectoCiclo1.maven.ProyectoCiclo1 to javafx.fxml;
     exports org.ProyectoCiclo1.maven.ProyectoCiclo1;
