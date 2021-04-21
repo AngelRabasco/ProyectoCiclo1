e1d6cf8df82e91d7535bf3908ab164d1dc62d860
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-21 10:08:01 +0200

    Added getter and setter for owner

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/ISubject.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/ISubject.java
@@ -1,10 +1,14 @@
 package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces;
 
+import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Users.User;
+
 public interface ISubject {
 	
 	public Integer getID();
 	public String getName();
+	public User getOwner();
 	public void setID(Integer ID);
 	public void setName(String name);
+	public void setOwner(User owner);
 	
 }
