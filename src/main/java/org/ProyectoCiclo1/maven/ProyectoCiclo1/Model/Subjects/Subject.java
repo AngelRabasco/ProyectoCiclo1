6e86c74cf5bec18e958115fec022fd74972838f7
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-21 10:13:44 +0200

    Prepared class for DAO

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Subjects/Subject.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Subjects/Subject.java
@@ -1,17 +1,25 @@
 package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Subjects;
 
 import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces.ISubject;
+import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Users.User;
 
 public class Subject implements ISubject {
-	private Integer ID;
-	private String name;
+	protected Integer ID;
+	protected String name;
+	protected User owner;
+	
 	
 	public Subject() {
-		this(-1,"");
+		this(-1,"",new User());
 	}
-	public Subject(Integer ID, String name) {
+	public Subject(Integer ID, String name, User owner) {
 		this.ID=ID;
 		this.name=name;
+		this.owner=owner;
+	}
+	public Subject(String name, User owner) {
+		this.name=name;
+		this.owner=owner;
 	}
 
 	public Integer getID() {
@@ -20,10 +28,16 @@ public class Subject implements ISubject {
 	public String getName() {
 		return this.name;
 	}
+	public User getOwner() {
+		return this.owner;
+	}
 	public void setID(Integer ID) {
 		this.ID=ID;
 	}
 	public void setName(String name) {
 		this.name=name;
+	}
+	public void setOwner(User owner) {
+		this.owner=owner;
 	}
 }
