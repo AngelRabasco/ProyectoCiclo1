d0e0f016336003e6dd5fd4607a48c0bd750ebbb8
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-23 10:48:47 +0200

    Get classes ready for DAO

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Entries/Entry.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Entries/Entry.java
@@ -6,18 +6,31 @@ import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Subjects.Subject;
 import java.time.LocalDateTime;
 
 public class Entry implements IEntry {
-	private Integer ID;
-	private String name;
-	private Subject subject;
-	private LocalDateTime creationDate;
+	protected Integer ID;
+	protected String name;
+	protected String description;
+	protected Subject subject;
+	protected LocalDateTime creationDate;
+	protected LocalDateTime lastEdited;
+	
 	
 	public Entry() {
-		
+		this(-1, "", "", new Subject(), LocalDateTime.now(), LocalDateTime.now());
 	}
-	public Entry(Integer ID, String name) {
+	public Entry(Integer ID, String name, String description, Subject subject,LocalDateTime creation, LocalDateTime edited) {
 		this.ID=ID;
 		this.name=name;
-		this.creationDate=LocalDateTime.now();
+		this.description=description;
+		this.subject=subject;
+		this.creationDate=creation;
+		this.lastEdited=edited;
+	}
+	public Entry(String name, String description, Subject subject,LocalDateTime creation, LocalDateTime edited) {
+		this.name=name;
+		this.description=description;
+		this.subject=subject;
+		this.creationDate=creation;
+		this.lastEdited=edited;
 	}
 	
 	public Integer getID() {
@@ -26,16 +39,38 @@ public class Entry implements IEntry {
 	public String getName() {
 		return this.name;
 	}
+	public String getDescription() {
+		return this.description;
+	}
 	public Subject getSubject() {
 		return this.subject;
 	}
 	public LocalDateTime getCreationDate() {
 		return this.creationDate;
 	}
+	public LocalDateTime getLastEdited() {
+		return this.lastEdited;
+	}
+	public void setID(Integer ID) {
+		this.ID=ID;
+	}
 	public void setName(String name) {
 		this.name=name;
 	}
+	public void setDescription(String description) {
+		this.description=description;
+	}
 	public void setSubject(Subject subject) {
 		this.subject=subject;
+	}
+	public void setCreationDate(LocalDateTime date) {
+		this.creationDate=date;
+	}
+	public void setLastEdited(LocalDateTime date) {
+		this.lastEdited=date;
+	}
+	
+	public String toString() {
+		return "Entry [ID=" + ID + ", name=" + name + ", description=" + description + ", subject=" + subject + ", creationDate=" + creationDate + ", lastEdited=" + lastEdited + "]";
 	}
 }
--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Entries/Reminder.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Entries/Reminder.java
@@ -1,29 +1,31 @@
 package org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Entries;
 
 import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Interfaces.IReminder;
+import org.ProyectoCiclo1.maven.ProyectoCiclo1.Model.Subjects.Subject;
+
 import java.time.LocalDateTime;
 
 public class Reminder extends Entry implements IReminder {
-	private LocalDateTime date;
+	private LocalDateTime remindDate;
 	private Integer status;
 	
 	public Reminder() {
 		super();
 	}
-	public Reminder(Integer ID, String name, LocalDateTime date) {
-		super(ID,name);
-		this.date=date;
+	public Reminder(Integer ID, String name, String description, Subject subject, LocalDateTime creation, LocalDateTime edited, LocalDateTime date) {
+		super(ID, name, description, subject, creation, edited);
+		this.remindDate=date;
 		this.status=0;
 	}
 	
 	public LocalDateTime getDate() {
-		return this.date;
+		return this.remindDate;
 	}
 	public Integer getStatus() {
 		return this.status;
 	}
 	public void setDate(LocalDateTime date) {
-		this.date=date;
+		this.remindDate=date;
 	}
 	public void setStatus(Integer Status) {
 		this.status=Status;
