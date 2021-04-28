0710192485bb06b237493386f72889310d8d7e53
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-28 11:10:20 +0200

    Revert "Changed Status from Integer to Boolean"
    This reverts commit d63172b235dadc00ef3ca5353350ba0e3d51b5d2.

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IReminder.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IReminder.java
@@ -5,8 +5,8 @@ import java.time.LocalDateTime;
 public interface IReminder {
 	
 	public LocalDateTime getDate();
-	public Boolean getStatus();				//Returns false (Pending) or true (Done)
+	public Integer getStatus();				//Returns 0 (Pending) or 1 (Done)
 	public void setDate(LocalDateTime date);
-	public void setStatus(Boolean Status);
+	public void setStatus(Integer Status);
 	
 }
