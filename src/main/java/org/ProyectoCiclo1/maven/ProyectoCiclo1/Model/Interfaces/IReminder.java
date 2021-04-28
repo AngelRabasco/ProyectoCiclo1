735b269dfdc77c5bdf0115c5b3c38ee112d62776
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-04-28 11:11:35 +0200

    Updated comment

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IReminder.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/Model/Interfaces/IReminder.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public interface IReminder {
 	
 	public LocalDateTime getDate();
-	public Integer getStatus();				//Returns 0 (Pending) or 1 (Done)
+	public Integer getStatus();				//Returns 0 (Not reminder), 1 (Pending) or 2 (Done)
 	public void setDate(LocalDateTime date);
 	public void setStatus(Integer Status);
 	
