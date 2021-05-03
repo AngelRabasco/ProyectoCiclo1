b76df1a3f4875d6deacaae0e9bd4a29e7ad3bf9e
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-05-03 10:45:28 +0200

    Made javafx.controls requirement transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module org.ProyectoCiclo1.maven.ProyectoCiclo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
+	requires transitive javafx.controls;
+	requires javafx.fxml;
 	requires java.sql;
-
-    opens org.ProyectoCiclo1.maven.ProyectoCiclo1 to javafx.fxml;
-    exports org.ProyectoCiclo1.maven.ProyectoCiclo1;
+	
+	opens org.ProyectoCiclo1.maven.ProyectoCiclo1 to javafx.fxml;
+	exports org.ProyectoCiclo1.maven.ProyectoCiclo1;
 }
