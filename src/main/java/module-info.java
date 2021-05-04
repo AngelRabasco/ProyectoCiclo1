4001634e74a18980fba9c5701dc12f7c8b94858a
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-05-04 10:56:26 +0200

    Added requirement for Spring Security Core

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module org.ProyectoCiclo1.maven.ProyectoCiclo1 {
 	requires transitive javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
-	
+	requires spring.security.core;
+
 	opens org.ProyectoCiclo1.maven.ProyectoCiclo1 to javafx.fxml;
 	exports org.ProyectoCiclo1.maven.ProyectoCiclo1;
 }
