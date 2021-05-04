8970be63b0ef85e061d4f682ee9c63b1bda284a7
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-05-04 13:59:58 +0200

    Added Requires com.jfoenix;

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.ProyectoCiclo1.maven.ProyectoCiclo1 {
 	requires transitive javafx.controls;
 	requires javafx.fxml;
+	requires com.jfoenix;
 	requires java.sql;
 	requires spring.security.core;
 
