add51cf3632c6b211134d171664224e43f90543f
AngelRabasco <82035161+AngelRabasco@users.noreply.github.com>
2021-05-16 12:09:22 +0200

    Commenting

--- src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/SubjectEditorController.java
+++ src/main/java/org/ProyectoCiclo1/maven/ProyectoCiclo1/SubjectEditorController.java
@@ -28,6 +28,7 @@ public class SubjectEditorController {
 	
 	@FXML
 	public void setFields(Subject subject, UserDAO owner, Boolean mode) {
+		//Carga las variables de la asignatura en los campos
 		addOrEdit=mode;
 		user=owner;
 		subjectID=subject.getID();
@@ -36,12 +37,15 @@ public class SubjectEditorController {
 	
 	@FXML
 	private void save() {
+	//Guarda la asignatura en la base de datos
 		if(addOrEdit==false) {
+		//Entra por este IF si estamos cargando el editor con el botón "Añadir"
 			if(nameField.getText().trim()!="") {
 				SubjectDAO subject=new SubjectDAO(null,
 						nameField.getText(),
 						user);
 				if(SubjectDAO.checkExists(subject.getName().toString(), user)==false) {
+					//Si la asignatura no existe la guarda y muestra una notificación
 					subject.save();
 					Alert alert=new Alert(Alert.AlertType.INFORMATION);
 					alert.setHeaderText(null);
@@ -50,6 +54,7 @@ public class SubjectEditorController {
 					alert.showAndWait();
 					closeWindow();
 				}else{
+					//Si existe muestra una advertencia
 					Alert alert=new Alert(Alert.AlertType.ERROR);
 					alert.setHeaderText(null);
 					alert.setTitle("Error de registro");
@@ -57,6 +62,7 @@ public class SubjectEditorController {
 					alert.showAndWait();
 				}
 			}else{
+				//Si el campo del nombre está vacio muestra una advertencia
 				Alert alert=new Alert(Alert.AlertType.ERROR);
 				alert.setHeaderText(null);
 				alert.setTitle("Error de registro");
@@ -64,11 +70,13 @@ public class SubjectEditorController {
 				alert.showAndWait();
 			}
 		}else{
+			//Entra por este ELSE si estamos cargando el editor con el botón "Editar"
 			if(nameField.getText().trim()!="") {
 				SubjectDAO subject=new SubjectDAO(subjectID,
 						nameField.getText(),
 						user);
 				if(SubjectDAO.checkExists(subject.getName().toString(), user)==false) {
+					//Si existe no la guarda
 					subject.save();
 					Alert alert=new Alert(Alert.AlertType.INFORMATION);
 					alert.setHeaderText(null);
@@ -77,6 +85,7 @@ public class SubjectEditorController {
 					alert.showAndWait();
 					closeWindow();
 				}else{
+					//Si no existe la guarda
 					Alert alert=new Alert(Alert.AlertType.ERROR);
 					alert.setHeaderText(null);
 					alert.setTitle("Error de actualización");
@@ -84,6 +93,7 @@ public class SubjectEditorController {
 					alert.showAndWait();
 				}
 			}else{
+				//Si el campo del nombre está vacio muestra una advertencia
 				Alert alert=new Alert(Alert.AlertType.ERROR);
 				alert.setHeaderText(null);
 				alert.setTitle("Error de actualización");
@@ -95,6 +105,7 @@ public class SubjectEditorController {
 	
 	@FXML
 	private void closeWindow() {
+		//Cierra la ventana
 		Stage currentStage=(Stage) saveButton.getScene().getWindow();
 		currentStage.close();
 	}
